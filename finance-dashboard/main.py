f78564f1aacb552059872ff9a124ce9336a8b2a2
Yan <87994542+yanomaly@users.noreply.github.com>
2024-12-19 03:27:09 +0300

    Fin dashboard data loading fix (#11)

--- finance-dashboard/main.py
+++ finance-dashboard/main.py
@@ -1,15 +1,24 @@
-import writer as wf
-import writer.ai
-import pandas as pd
-from prompts import stock_prompts, income_prompts, earnings_prompt
-from stock_data import download_data, download_sp500, stock_news, _one_day_data, income_statement, earnings_calls
-from charts import update_scatter_chart, handle_click
-from dotenv import load_dotenv
 import os
 
-load_dotenv() 
+import pandas as pd
+import writer as wf
+import writer.ai
+from charts import handle_click, update_scatter_chart
+from dotenv import load_dotenv
+from prompts import earnings_prompt, income_prompts, stock_prompts
+from stock_data import (
+    _one_day_data,
+    download_data,
+    download_sp500,
+    earnings_calls,
+    income_statement,
+    stock_news,
+)
+
+load_dotenv()
 
 writer.ai.init(os.getenv("WRITER_API_KEY"))
+
 
 # Update all data
 def updates(state):
@@ -23,6 +32,7 @@ def updates(state):
     _one_day_data(state)
     _refresh_window(state)
 
+
 # Refresh the window
 def _refresh_window(state):
     state["show_income_metrics"]["visible"] = False
@@ -31,47 +41,61 @@ def _refresh_window(state):
     state["show_analysis_text"]["language"] = False
     state["message"] = "Writer AI insights will be generated here"
 
+
 # Summarize earnings call using Palmyra-Fin model
 def summarize_earnings(state):
-    _refresh_window(state)  
+    _refresh_window(state)
     state["message"] = f"% {state['symbol']} earnings call will be summarized here"
-    
+
     earnings_transcript = state["earnings_transcript"]
     prompt = earnings_prompt.format(earnings_transcript=earnings_transcript)
-    submission = writer.ai.complete(prompt, config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192})
+    submission = writer.ai.complete(
+        prompt,
+        config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192},
+    )
     state["message"] = f"+ {state['symbol']} earnings call summary"
     state["analysis"] = submission.strip()
     state["show_analysis_text"]["visible"] = True
-    
-def prompt_parameters_words(state,payload):
+
+
+def prompt_parameters_words(state, payload):
     state["prompt_parameters_words"] = payload
     _refresh_window(state)
-    
-def prompt_parameters_lang(state,payload):
-    state["prompt_parameters_lang"] = payload
-    generate_stock_analysis(state)
-    
-def generate_stock_analysis(state):
-    _refresh_window(state)  
-    if state["prompt_parameters_lang"] == "":
-        state["prompt_parameters_lang"] == "English"
 
-    state["message"] = f"% {state['symbol']} trends will be analyzed here in {state['prompt_parameters_lang']}"
+
+def handle_prompt_parameters_lang(state):
+    generate_stock_analysis(state)
+
+
+def generate_stock_analysis(state):
+    _refresh_window(state)
+
+    state["message"] = (
+        f"% {state['symbol']} trends will be analyzed here in {state['prompt_parameters_lang']}"
+    )
     stock_name = state["symbol"]
     stock_data = state["main_df"][:365]
-        
+
     rounded_value = round(state["prompt_parameters_words"], 0)
     language = state["prompt_parameters_lang"]
     # Convert the rounded value to an integer
     integer_value = int(rounded_value)
 
-    prompt = stock_prompts.format(language=language, stock_name=stock_name,words=integer_value,stock_data=stock_data)
-    submission = writer.ai.complete(prompt, config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192})
+    prompt = stock_prompts.format(
+        language=language,
+        stock_name=stock_name,
+        words=integer_value,
+        stock_data=stock_data,
+    )
+    submission = writer.ai.complete(
+        prompt,
+        config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192},
+    )
     state["analysis"] = submission.strip()
     state["message"] = f"+ {state['symbol']} trends analyzed"
     state["show_analysis_text"]["visible"] = True
     state["show_analysis_text"]["language"] = True
-        
+
     return submission
 
 
@@ -86,7 +110,10 @@ def generate_income_analysis(state):
         stock_data=stock_data,
         stock_name=stock_name,
     )
-    submission = writer.ai.complete(prompt, config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192})
+    submission = writer.ai.complete(
+        prompt,
+        config={"model": "palmyra-fin-32k", "temperature": 0.7, "max_tokens": 8192},
+    )
     state["analysis"] = submission.strip()
     state["message"] = f"+ {state['symbol']} income statement visualized"
     state["show_income_metrics"]["visible"] = True
@@ -94,13 +121,16 @@ def generate_income_analysis(state):
     state["show_bar_graph"]["visible"] = True
     return submission
 
+
 def stock_tags(state, payload):
     state["symbol"] = payload
     updates(state)
 
+
 def _get_main_df(filename):
     main_df = pd.read_csv(filename)
-    return main_df 
+    return main_df
+
 
 initial_state = wf.init_state(
     {
@@ -127,10 +157,7 @@ initial_state = wf.init_state(
                 "url": "https://finance.yahoo.com/news/microsoft-corporation-msft-attracting-investor-130015018.html",
             },
         },
-        "show_analysis_text": {
-            "visible": False,
-            "language": False
-        },
+        "show_analysis_text": {"visible": False, "language": False},
         "show_income_metrics": {
             "visible": False,
         },
